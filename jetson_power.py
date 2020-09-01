0175f70c227cf400afbc430cc898947acc519824
Hugo Hadfield <hadfield.hugo@gmail.com>
2020-09-01 16:52:42 +0100

    Added a file echo

--- jetson_power.py
+++ jetson_power.py
@@ -1,6 +1,8 @@
 
 import time
 
+output_file = 'power.txt'
+
 rail_names = {
 "VDD_IN": "/sys/bus/i2c/drivers/ina3221x/0-0041/iio:device1/in_power0_input",
 "VDD_SYS_GPU": "/sys/bus/i2c/drivers/ina3221x/0-0040/iio:device0/in_power0_input",
@@ -10,25 +12,33 @@ rail_names = {
 "VDD_4V0_WIFI": "/sys/bus/i2c/drivers/ina3221x/0-0040/iio:device0/in_power2_input"
 }
 
-def loop():
+def loop(file_echo=None):
     for rail, file in rail_names.items():
         with open(file, "r") as fobj:
             milliwatts = int(fobj.readline())
             print(milliwatts, end='\t')
+            if file_echo is not None:
+                print(milliwatts, end='\t', file=file_echo)
     print()
 
 
-def print_titles():
+def print_titles(file_echo=None):
     for rail, file in rail_names.items():
         print(rail, end='\t')
+        if file_echo is not None:
+            print(rail, end='\t', file=file_echo)
     print()
 
 
 if __name__ == '__main__':
-    print('Time', end='\t')
-    print_titles()
-    tstart = time.time()
-    while True:
-        time.sleep(1.0)
-        print(time.time() - tstart, end='\t')
-        loop()
+    with open(output_file, 'w') as ofobj:
+        print('Time', end='\t')
+        print('Time', end='\t', file=ofobj)
+        print_titles(file_echo=ofobj)
+        tstart = time.time()
+        while True:
+            time.sleep(1.0)
+            this_time = time.time() - tstart
+            print(this_time, end='\t')
+            print(this_time, end='\t', file=ofobj)
+            loop(file_echo=ofobj)
